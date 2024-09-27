92420d5be3e706319c32ca4514e6ea1a2dc67f5b
Toha <39374275+tohabin@users.noreply.github.com>
2024-09-27 10:52:47 +0600

    route for profile controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Profile/{id}', [ProfileController::class, 'Index']);
+
+
